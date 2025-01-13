235446a171bb8a0234c8cb0cfe995a21a5bcfe81
Edward Cheong <edloischeong@gmail.com>
2025-01-13 16:37:58 +0800

    added buffer polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig(() => {
@@ -11,6 +12,15 @@ export default defineConfig(() => {
         '@': path.resolve(__dirname, './src'),
       },
     },
-    plugins: [TanStackRouterVite(), react()],
+    plugins: [
+      TanStackRouterVite(),
+      react(),
+      nodePolyfills({
+        include: ['path'],
+        globals: {
+          Buffer: true,
+        },
+      }),
+    ],
   }
 })
